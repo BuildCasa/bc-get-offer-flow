1347574b392bd7f3415ad46b9d4b3981efbf20f9
Kyle Kilat <kkilat@users.noreply.github.com>
2024-11-01 16:00:01 -0700

    Remove personalized phone number functionality

--- src/modules/models/PersonalizationViewModel.js
+++ src/modules/models/PersonalizationViewModel.js
@@ -11,12 +11,6 @@ import { fetchUserGeo } from '../services/GeoJSGeolocationService'
  * Constants
  * ----------------------------------------------------------------
  */
-const BC_MARKET_PHONE_NUMBERS = {
-  DEFAULT: '(415) 941-5861',
-  Sacramento: '(916) 619-1442',
-  'Bay Area': '(415) 941-5861',
-  SoCal: '(213) 322-1360',
-}
 
 /*
  * ----------------------------------------------------------------
@@ -30,33 +24,12 @@ function createPersonalizationViewModel() {
     get market() {
       return getMarketForCity(this.userGeo.city, this.marketsData)
     },
-    get bcPhoneNumber() {
-      return getBcPhoneNumberForCity(this.userGeo.city, this.marketsData)
-    },
-    get bcPhoneNumberHref() {
-      return `tel:+1${this.bcPhoneNumber.replace(/\D/g, '')}`
-    },
     async init() {
       this.marketsData = marketCities
 
       this.userGeo = await fetchUserGeo()
     },
   }
-}
-
-/**
- * Given a city and marketsData definition object, returns the appropriate BuildCasa phone number to display
- * Ideally, localized to the market, but defaults to a generic number if no market match
- */
-function getBcPhoneNumberForCity(city, marketsData) {
-  const defaultPhoneNumber = BC_MARKET_PHONE_NUMBERS.DEFAULT
-
-  if (!city || typeof city !== 'string') return defaultPhoneNumber
-
-  const market = getMarketForCity(city, marketsData)
-  const marketSpecificPhoneNumber = getBcPhoneNumberForMarket(market)
-
-  return marketSpecificPhoneNumber ?? defaultPhoneNumber
 }
 
 /**
@@ -72,21 +45,6 @@ function getMarketForCity(city, marketsData) {
       ).length > 0
     ) {
       return marketKey
-    }
-  }
-
-  return null
-}
-
-/**
- * Given a 'market' and marketsData definition object, returns a location-specific BuildCasa phone number to display, if found
- */
-function getBcPhoneNumberForMarket(market) {
-  if (!market || typeof market !== 'string') return null
-
-  for (const marketKey of Object.keys(BC_MARKET_PHONE_NUMBERS)) {
-    if (marketKey.toLowerCase().trim() === market.toLowerCase().trim()) {
-      return BC_MARKET_PHONE_NUMBERS[marketKey] ?? null
     }
   }
 
