5be4ae4375e9e5b50b7248a23846727635fa8be0
Kyle Kilat <kkilat@users.noreply.github.com>
2024-11-19 16:20:29 -0800

    Update THCalculatorViewModel to be reactive to personalization updates

--- src/modules/models/THCalculatorViewModel.js
+++ src/modules/models/THCalculatorViewModel.js
@@ -6,24 +6,69 @@
 function createTHCalculatorViewModel(personalizationViewModel) {
   return {
     purchasePrice: null,
-    maxPurchasePrice: null,
-    minPurchasePrice: null,
-    inputStep: null,
     commissionRate: 0.025,
-    flatFee: null,
 
     init: function () {
       this.purchasePrice = personalizationViewModel.getContent(
         'calcDefaultPurchasePrice',
       )
-      this.maxPurchasePrice = personalizationViewModel.getContent(
-        'calcMaxPurchasePrice',
-      )
-      this.minPurchasePrice = personalizationViewModel.getContent(
-        'calcMinPurchasePrice',
-      )
-      this.inputStep = personalizationViewModel.getContent('calcInputStep')
-      this.flatFee = personalizationViewModel.getContent('calcFlatFee')
+    },
+
+    /**
+     * Computed property that returns the value of the calcDefaultPurchasePrice key in the personalizationViewModel
+     *
+     * Add Alpine attribute to calc slider input element to update input value when this changes:
+     * x-init=$watch('$store.thCalculatorViewModel.defaultPurchasePrice', (newVal, oldVal) => $store.thCalculatorViewModel.purchasePrice = newVal)
+     *
+     * @type {number}
+     */
+    get defaultPurchasePrice() {
+      return personalizationViewModel.getContent('calcDefaultPurchasePrice')
+    },
+
+    /**
+     * Computed property that returns the value of the calcMaxPurchasePrice key in the personalizationViewModel
+     *
+     * Add Alpine attribute to calc slider input element to set max value of input element:
+     * x-bind:max=$store.thCalculatorViewModel.maxPurchasePrice
+     *
+     * @type {number}
+     */
+    get maxPurchasePrice() {
+      return personalizationViewModel.getContent('calcMaxPurchasePrice')
+    },
+
+    /**
+     * Computed property that returns the value of the calcMinPurchasePrice key in the personalizationViewModel
+     *
+     * Add Alpine attribute to calc slider input element to set min value of input element:
+     * x-bind:min=$store.thCalculatorViewModel.minPurchasePrice
+     *
+     * @type {number}
+     */
+    get minPurchasePrice() {
+      return personalizationViewModel.getContent('calcMinPurchasePrice')
+    },
+
+    /**
+     * Computed property that returns the value of the calcInputStep key in the personalizationViewModel
+     *
+     * Add Alpine attribute to calc slider input element to set step value of input element:
+     * x-bind:step=$store.thCalculatorViewModel.inputStep
+     *
+     * @type {number}
+     */
+    get inputStep() {
+      return personalizationViewModel.getContent('calcInputStep')
+    },
+
+    /**
+     * Computed property that returns the value of the calcFlatFee key in the personalizationViewModel
+     *
+     * @type {number}
+     */
+    get flatFee() {
+      return personalizationViewModel.getContent('calcFlatFee')
     },
 
     get formattedPurchasePrice() {
