d7a4c9a984f3059705a8497300edc23215bd0425
Kyle Kilat <kkilat@users.noreply.github.com>
2025-01-21 23:02:32 -0800

    Renamed WeBuyCALots ViewModel modules

--- src/webuycalots.js
+++ src/webuycalots.js
@@ -24,9 +24,9 @@ import {
   createFlowUIHelpers,
 } from './modules/flows/WeBuyCALotsFlows'
 
-import { createAddressViewModel } from './modules/models/AddressViewModel'
-import { createContactViewModel } from './modules/models/ContactViewModel'
-import { createEstimateViewModel } from './modules/models/EstimateViewModel'
+import { createAddressViewModel } from './modules/models/WeBuyCALotsAddressViewModel'
+import { createContactViewModel } from './modules/models/WeBuyCALotsContactViewModel'
+import { createEstimateViewModel } from './modules/models/WeBuyCALotsEstimateViewModel'
 import { createPersonalizationViewModel } from './modules/models/PersonalizationViewModel'
 import { createExperimentationViewModel } from './modules/models/ExperimentationViewModel'
 
